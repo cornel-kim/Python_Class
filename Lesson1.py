3035b4961ae3ac0b5660343d1f3ec61ba960e895
cornel-kim <cornelkemboi@yahoo.com>
2020-09-30 12:54:45 +0300

    first class commit

---
+++ Lesson1.py
@@ -0,0 +1,22 @@
+# print("Lesson 1")
+# print(2020)
+# print(2020.10)
+# integers, Floats, strings, Boolean, tuples, dictionaries, list
+# variables store information example student has a name and marks scored 380
+student = "john"#type string add double or single quotes
+print(type(student))
+marks = 380#variable of type integer
+print(type(marks))
+average = 70.5
+print(type(average))
+print("Student name", student,"marks scored", marks, "average mark is", average)
+#data type list
+list1 = ["john", "paul", "mark"]
+print(type(list1))
+#data type tuple
+tuple1 = ("john", "peter")
+print(type(tuple1))
+dict1 = {
+    1: "Mary"
+}
+print(type(dict1))
---
+++ lesson1a.py
@@ -0,0 +1,32 @@
+# x = 4
+# w = 5
+# print("x + w =", x + w)#addition
+# print("x - w =", x - w)#subtraction
+# print("x * w =", x * w)#multiplication
+# print("x /w =", x / w)#division
+# print("x ** w =", x ** w)#square
+# print("x // w =", x // w)#squareroot
+
+
+a = 40
+b = 60
+#checks true or false
+print("a > b", a > b)
+print("a < b", a < b)
+print("a >= b", a >= b)
+print("a <= b", a <= b)
+print("a == b", a == b)
+print("a != b", a != b)
+
+# z = x + w
+# #print function will calculate the sum
+# print(z)
+#
+#
+# mathematics = 60.0
+# english = 40.0
+# Kiswahili = 50.0
+# total_marks = mathematics + english + Kiswahili
+# print("Your total marks is equals to:", total_marks)
+# avarage_mark = total_marks/3
+# print("Your avarage marks is:", avarage_mark)

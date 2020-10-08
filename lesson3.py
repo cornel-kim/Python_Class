c7662adb4d4473a1492ec0b1d749b28762e36650
cornel-kim <cornelkemboi@yahoo.com>
2020-10-08 10:17:50 +0300

    first class commit

---
+++ lesson3.py
@@ -0,0 +1,24 @@
+#lists and tuples and loops
+# a list is a collection of objects in an array
+#Characteristics of a list is:- mutable, dynamic, ordered
+
+list1 = ['John', 'Peter', 'Mark', 'Mary']
+list2 = [1, 3, 6, 7, 4, 6, 'Fatuma']
+print(list1)
+list1[2] = 20
+print(list1)
+print(list2[0])#access the first item
+print(list2[1:5])#access items of an array in a range example range of 2 to 5
+print(list1[1:])#access items from index one to last
+print(list1[-1])#Access the last item of an array
+
+#tuple
+#characteristics: - immutable,
+tuple1 = ('Mangoes', 'Oranges', 'Bananas', 'Pineapple')
+
+print(tuple1)
+print(tuple1)
+print(tuple1[0])#access the first item
+print(tuple1[1:3])#access items of an array in a range example range of 2 to 5
+print(tuple1[1:])#access items from index one to last
+print(tuple1[-1])#Access the last item of an array

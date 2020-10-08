ca8141af5fed646c18c27bf965b03093c36d3cd5
cornel-kim <cornelkemboi@yahoo.com>
2020-10-08 10:34:31 +0300

    first class commit

---
+++ lesson3a.py
@@ -0,0 +1,12 @@
+#Access items in an array i.e list or tuples we use loops. While loop and for loop
+#while loop
+x = 11
+while x < 100:#condition has array of items from 0 to 9(0, 1, 2,3, 4, 5, 6, 7, 8, 9)
+
+    if x > 200:
+        print(x)
+    else:
+        print("number not found")
+    x = x + 10
+
+# for loop
---
+++ lesson3b.py
@@ -0,0 +1,14 @@
+#For loop
+
+# for x in range(0, 4):
+#     print(x)
+
+
+list1 = ['John', 'Peter', 'Mark', 'Mary']
+for name in list1:
+    print(name)
+
+
+student_name = "Kimwele"
+for char in student_name:
+    print(char)
